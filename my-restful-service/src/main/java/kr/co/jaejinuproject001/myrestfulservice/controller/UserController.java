dc22ac3d324cd4402563af08d4297813049e3482
jaejinu <dbwowls12345@gmail.com>
2025-07-16 08:01:17 +0900

    get > find > null chk > exception

--- my-restful-service/src/main/java/kr/co/jaejinuproject001/myrestfulservice/controller/UserController.java
+++ my-restful-service/src/main/java/kr/co/jaejinuproject001/myrestfulservice/controller/UserController.java
@@ -2,10 +2,12 @@ package kr.co.jaejinuproject001.myrestfulservice.controller;
 
 import kr.co.jaejinuproject001.myrestfulservice.bean.User;
 import kr.co.jaejinuproject001.myrestfulservice.dao.UserDaoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import kr.co.jaejinuproject001.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,8 +23,29 @@ public class UserController {
         return service.findAll();
     }
 
+
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable Integer id) {
-        return service.findOne(id);
+     public User retrieveUser(@PathVariable Integer id) {
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
+
     }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(savedUser);
+    }
+
+
 }
